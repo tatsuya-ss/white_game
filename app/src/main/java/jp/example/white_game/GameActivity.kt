898b563ef96a4dffa78e5dab09ae4abe80a937c9
tatsuya-ss <tatsu.knight.soccer@ezweb.ne.jp>
2022-10-15 10:25:18 +0900

    Activity名を変更

--- app/src/main/java/jp/example/white_game/GameActivity.kt
+++ app/src/main/java/jp/example/white_game/GameActivity.kt
@@ -3,9 +3,9 @@ package jp.example.white_game
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_game)
     }
 }
